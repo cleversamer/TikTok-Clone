720ea4d633c92828c9930efdc1708fca3e281418
Samer A <thedev.samer@gmail.com>
2022-04-03 16:11:03 +0300

    finished the app design

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,16 @@ const App = () => {
           comments="2399"
           shares="6926"
         />
+
+        <Video
+          videoUrl="https://v16-webapp.tiktok.com/ac079543c46b56b5c9cddaf526b8dd94/6249e664/video/tos/alisg/tos-alisg-pve-0037c001/00055a3ea82447339db2de9ee600d943/?a=1988&br=1752&bt=876&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3A7nz7ThCYGqDXq&l=2022040312242501024524720324AC8CD5&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=ajc8NTo6ZmlwOzMzODczNEApZDo5PGc4aWRnNzdkaWg0ZGcxZXNvcjRvY3NgLS1kMS1zc2AtXy0xY2BfMGEwNjNhNl46Yw%3D%3D&vl=&vr="
+          channel="uzmarahil724"
+          description="How to draw side face #fyp #foryou"
+          song="JOVYNN Suave sound edit - jovynn"
+          likes="937.1K"
+          comments="2399"
+          shares="6926"
+        />
       </div>
     </div>
   );
--- src/components/VideoSidebar.jsx
+++ src/components/VideoSidebar.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { IconButton } from "@mui/material";
+import { Favorite, FavoriteBorder, Message, Share } from "@mui/icons-material";
+import "../css/video-sidebar.css";
 
 const VideoSidebar = ({ likes, comments, shares }) => {
+  const [isLiked, setLiked] = useState(false);
+
   return (
-    <div>
-      <h1>VideoSideBar</h1>
+    <div className="video-sidebar">
+      <IconButton onClick={() => setLiked(!isLiked)}>
+        <div className="video-sidebar__button">
+          {isLiked ? <Favorite /> : <FavoriteBorder />}
+          <p>{likes}</p>
+        </div>
+      </IconButton>
+
+      <IconButton>
+        <div className="video-sidebar__button">
+          <Message />
+          <p>{comments}</p>
+        </div>
+      </IconButton>
+
+      <IconButton>
+        <div className="video-sidebar__button">
+          <Share />
+          <p>{shares}</p>
+        </div>
+      </IconButton>
     </div>
   );
 };
