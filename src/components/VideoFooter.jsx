a815c1044cd46dc18b1cd168abc7a9134bc0ea08
Samer A <thedev.samer@gmail.com>
2022-04-03 14:26:50 +0300

    added the video footer

--- src/components/VideoFooter.jsx
+++ src/components/VideoFooter.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import "../css/video-footer.css";
 
 const VideoFooter = () => {
   return (
-    <div>
-      <h1>VideoFooter</h1>
+    <div className="video-footer">
+      <div className="video-footer__text">
+        <h3>@username</h3>
+        <p>this is a description</p>
+      </div>
+
+      <img
+        src="https://static.thenounproject.com/png/934821-200.png"
+        alt="record"
+        className="video-footer__record"
+      />
     </div>
   );
 };
